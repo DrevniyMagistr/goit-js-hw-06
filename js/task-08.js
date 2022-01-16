47710f0fd508b8726b6d71be8829916aa0c43f7e
DrevniyMagistr <sasha.klishch@gmail.com>
2022-01-16 19:52:53 +0200

    fix 8 question

--- js/task-08.js
+++ js/task-08.js
@@ -6,10 +6,10 @@ const onFormSubmit = (e) => {
   const email = e.currentTarget.elements.email.value;
   const password = e.currentTarget.elements.password.value;
 
-  console.log(`email: ${email}, password: ${password}`);
   email && password !== ''
     ? formRef.reset()
     : alert('Все поля должны быть заполнены!!!');
+  console.log(`email: ${email}, password: ${password}`);
 };
 
 formRef.addEventListener('submit', onFormSubmit);
